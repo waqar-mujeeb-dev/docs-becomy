e72a14693d0e7f1414203b771f4499ca8ec99dc0
waqar-mujeeb-dev <waqar.mujeeb.dev@gmail.com>
2024-02-07 19:56:24 +0500

    changed config to update url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: '/docs-becomy/',
+  url: 'https://https://waqar-mujeeb-dev.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'docs-becomy',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
