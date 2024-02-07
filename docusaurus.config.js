8656c36581de89b86d669fb5e92f0fe8f42fffa3
waqar-mujeeb-dev <waqar.mujeeb.dev@gmail.com>
2024-02-07 19:25:22 +0500

    add deployment branch name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'EquipTechnique', // Usually your GitHub org/user name.
   projectName: 'docs-becomy.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
