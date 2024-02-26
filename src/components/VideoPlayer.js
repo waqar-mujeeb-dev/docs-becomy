1baf971be213d4fd5df62d6177a3ebe53c256619
waqar-mujeeb-dev <waqar.mujeeb.dev@gmail.com>
2024-02-26 22:05:23 +0500

    misc changes

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const VideoPlayer = ({url}) => {
+  return (
+    <div style={{ position: 'relative', height: 0, width: '100%', paddingBottom: '43.020833333333336%' }}>
+      <iframe 
+        src={url} frameBorder="0" 
+        style={{position:"absolute",width:"100%", height:"100%",borderRadius:"6px",left:"0",top:"0"}} 
+        allowfullscreen="">
+      </iframe>
+    </div>
+  )
+}
